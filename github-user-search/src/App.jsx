770b0fdcd69b7b22a4ea0448662f04bcfba86120
namaromebecky <namaromebecky@gmail.com>
2025-11-25 15:06:57 +0300

    feat: Add github-user-search React project directory

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GitHub User Search</h1>
+        <p>Welcome to the GitHub User Search Application</p>
+        <p>Start building your components in the components directory!</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
